5a62de91b75d2d08754af826ed9c4deae33674ed
Anatoly Scherbakov <anatoly@criterion.net>
2019-08-13 13:11:50 +0700

    We can now read the file

--- notion_show.py
+++ notion_show.py
@@ -1,5 +1,29 @@
+import logging
+import zipfile
+from glob import glob
+from typing import Optional
+
+from fire import Fire
+
+logger = logging.getLogger(__name__)
+
+
+def process(archive_path: str = None, output_path: Optional[str] = None):
+    archive_path = glob(archive_path)[0]
+
+    logger.info('Source file: %s found.', archive_path)
+
+    archive = zipfile.ZipFile(archive_path)
+    for filename in archive.namelist():
+        content = archive.read(filename)
+
+        print(content)
+
+
 def main():
-    pass
+    return Fire({
+        'process': process
+    })
 
 
 if __name__ == '__main__':
